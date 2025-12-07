ed57c1786a493c901da3301ee0dd89d538365306
Livius307 <livius.wiebe@gmail.com>
2025-12-07 11:39:19 +0100

    Initial commit

---
+++ src/livius/Main.java
@@ -0,0 +1,23 @@
+package livius;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String q1 = "who?";
+        String q2 = "what?";
+        String q3 = "where?";
+        String q4 = "why?";
+        String[] questions = new String[4];
+        questions[0] = "who?";
+        questions[1] = "what?";
+        questions[2] = "where?";
+        questions[3] = "why?";
+        String[] answers = new String[4];
+        answers[0] = "me";
+        answers[1] = "that";
+        answers[2] = "there";
+        answers[3] = "idk";
+        }
+    }
