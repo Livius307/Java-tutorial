b5ac34dff17a9b3e9f78b2d4779b344629dba36f
Livius307 <livius.wiebe@gmail.com>
2025-12-20 15:14:15 +0100

    learning object oriented programming

---
+++ src/livius/Dog.java
@@ -0,0 +1,20 @@
+package livius;
+
+public class Dog {
+    public String picture;
+    public String name;
+    public int age;
+
+    /*public Dog() {
+    } */
+
+    public Dog(String picture, String name, int age) {
+        this.picture = picture;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void birthday() {
+        age++;
+    }
+}
--- src/livius/Main.java
+++ src/livius/Main.java
@@ -1,59 +1,16 @@
 package livius;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        String q1 = "who?";
-        String q2 = "what?";
-        String q3 = "where?";
-        String q4 = "why?";
-        String[] questions = new String[4];
-        questions[0] = "who?";
-        questions[1] = "what?";
-        questions[2] = "where?";
-        questions[3] = "why?";
-        String[] answers = new String[4];
-        answers[0] = "me";
-        answers[1] = "that";
-        answers[2] = "there";
-        answers[3] = "idk";
+        Dog bengie = new Dog("bengie.png", "Bengie", 9 );
+        Dog jeremy = new Dog("jeremy.png", "Jeremy", 7 );
 
-        List<String> moreQuestions = new ArrayList<>();
-        moreQuestions.add("q4");
-        moreQuestions.add("q5");
-        moreQuestions.add("q4");
-        moreQuestions.add("q4");
-        System.out.println(moreQuestions.size());
-        System.out.println(moreQuestions.get(1));
-        System.out.println(moreQuestions.contains("q4"));
-        System.out.println(moreQuestions.isEmpty());
-        System.out.println(moreQuestions.lastIndexOf("q4"));
-        System.out.println(moreQuestions.size());
+        System.out.println("Bengie age: " + bengie.age);
+        System.out.println("Jeremy age: " + jeremy.age);
 
-        Set<String> countriesToVisit = new HashSet<>();
-        countriesToVisit.add("France");
-        countriesToVisit.add("Germany");
-        countriesToVisit.add("Spain");
-        countriesToVisit.add("France");
-        System.out.println(countriesToVisit);
+        bengie.birthday();
 
-        Map<String, Integer> countryToPopulation = new HashMap<>();
-        countryToPopulation.put("USA", 331000000);
-        countryToPopulation.put("United Kingdom", 67330000);
-        countryToPopulation.put("Austria", 8950000);
-        System.out.println(countryToPopulation.get("USA"));
-        System.out.println(countryToPopulation.get("Germany"));
-        System.out.println(countryToPopulation);
-
-
-
-        for (int i = 0; i < moreQuestions.size(); i++) {
-            System.out.println(moreQuestions.get(i));
-        }
-
-        for(Map.Entry<String, Integer> entry : countryToPopulation.entrySet()) {
-            System.out.println("Key: " + entry.getKey() + "  Value: " + entry.getValue());
-        }
+        System.out.println("Bengie age: " + bengie.age);
+        System.out.println("Jeremy age: " + jeremy.age);
     }
 }
